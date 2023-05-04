caa13110789611cc6ba7acec2c37ec0b04054a4d
David Nguyen <davido.nguyen1212@gmail.com>
2023-05-04 10:26:48 -0700

    started next fit algorithm

--- next_fit.py
+++ next_fit.py
@@ -3,4 +3,24 @@
 # explanations for member functions are provided in requirements.py
 
 def next_fit(items: list[float], assignment: list[int], free_space: list[float]):
-	pass
+	count = 0
+	i = 0
+	while (i < len(items)):
+		current = assignment[count]
+
+		if (items[i] + current <= 1):
+			assignment[count] += items[i]
+		else:
+			assignment[count + 1] += items[i]
+			count += 1
+
+		i += 1
+
+
+bin = [.5, .7, .5, .2, .4, .2, .5, .1, .6]
+assign = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+free = []
+
+next_fit(bin, assign, free)
+
+print(assign)
