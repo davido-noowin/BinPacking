4eb9fe61972dfdc2a300d4c352bcd54a2b2de4f0
David Nguyen <davido.nguyen1212@gmail.com>
2023-05-06 12:49:01 -0700

    fix next fit algorithm

--- next_fit.py
+++ next_fit.py
@@ -1,26 +1,40 @@
 # Example file: next_fit.py
-
+import sys
+import decimal
 # explanations for member functions are provided in requirements.py
 
 def next_fit(items: list[float], assignment: list[int], free_space: list[float]):
 	count = 0
 	i = 0
-	while (i < len(items)):
-		current = assignment[count]
+	
+	precision = decimal.Context(prec=20)
 
-		if (items[i] + current <= 1):
-			assignment[count] += items[i]
+	free_space.append(1)
+	while (i < len(items)):
+		current = free_space[count]
+
+		if (current - items[i] >= -sys.float_info.epsilon):
+			assignment[i] = count
+			free_space[count] -= items[i]
 		else:
-			assignment[count + 1] += items[i]
+			free_space.append(1)
 			count += 1
+			assignment[i] = count
+			free_space[count] -= items[i]
 
 		i += 1
 
+	for i in range(len(free_space)):
+		remain = decimal.Decimal(free_space[i])
+		modded = remain.quantize(decimal.Decimal('.000000000000001'), context=precision)
+		free_space[i] = float(modded.normalize())
 
-bin = [.5, .7, .5, .2, .4, .2, .5, .1, .6]
-assign = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+bin = [0.1, 0.8, 0.3, 0.5, 0.7, 0.2, 0.6, 0.4]
+assign = [0, 0, 0, 0, 0, 0, 0, 0]
 free = []
 
 next_fit(bin, assign, free)
 
 print(assign)
+print(free)
